c689c2e7539ea4c081657ee03b79175fa5a2ce27
haveneryck <haveneryck@gmail.com>
2025-09-14 21:24:29 +0000

    Adicionando estrutura que permite múltiplos ataques em loop até o jogador decidir parar.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,28 +13,36 @@ int main() {
     for (int i = 0; i < 3; i++) tabuleiro[5 + i][4 - i] = 3;
 
     int ataque_x, ataque_y;
-    printf("Digite as coordenadas do ataque (x y): ");
-    scanf("%d %d", &ataque_x, &ataque_y);
+    char continuar;
 
-    if (ataque_x < 0 || ataque_x >= 10 || ataque_y < 0 || ataque_y >= 10) {
-        printf("Coordenadas inválidas!\n");
-    } else if (tabuleiro[ataque_y][ataque_x] == 3) {
-        tabuleiro[ataque_y][ataque_x] = 9;
-        printf("Acertou um navio em (%d, %d)!\n", ataque_x, ataque_y);
-    } else if (tabuleiro[ataque_y][ataque_x] == 0) {
-        tabuleiro[ataque_y][ataque_x] = 1;
-        printf("Água em (%d, %d).\n", ataque_x, ataque_y);
-    } else {
-        printf("Essa posição já foi atacada.\n");
-    }
+    do {
+        printf("Digite as coordenadas do ataque (x y): ");
+        scanf("%d %d", &ataque_x, &ataque_y);
 
-    printf("\n=== Tabuleiro 10x10 ===\n");
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
+        if (ataque_x < 0 || ataque_x >= 10 || ataque_y < 0 || ataque_y >= 10) {
+            printf("Coordenadas inválidas!\n");
+        } else if (tabuleiro[ataque_y][ataque_x] == 3) {
+            tabuleiro[ataque_y][ataque_x] = 9;
+            printf("Acertou um navio em (%d, %d)!\n", ataque_x, ataque_y);
+        } else if (tabuleiro[ataque_y][ataque_x] == 0) {
+            tabuleiro[ataque_y][ataque_x] = 1;
+            printf("Água em (%d, %d).\n", ataque_x, ataque_y);
+        } else {
+            printf("Essa posição já foi atacada.\n");
         }
-        printf("\n");
-    }
+
+        printf("\n=== Tabuleiro 10x10 ===\n");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                printf("%d ", tabuleiro[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("Deseja atacar novamente? (s/n): ");
+        scanf(" %c", &continuar);
+
+    } while (continuar == 's' || continuar == 'S');
 
     return 0;
 }
