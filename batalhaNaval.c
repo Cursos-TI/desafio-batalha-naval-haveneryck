61aeaddac757b9d83e39daee51fa97b3ccfca49b
haveneryck <haveneryck@gmail.com>
2025-09-14 20:13:56 +0000

    Iniciando desenvolvimento do Nível Aventureiro com estrutura de tabuleiro 10x10

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    // Etapa 1: Criação do tabuleiro 5x5
-    int tabuleiro[5][5] = {0};
+    // Nível Novato – Tabuleiro 5x5
+    int tabuleiro_novato[5][5] = {0};
 
-    // Etapa 2: Definição das posições dos navios
     // Navio vertical (coluna fixa, linha variável)
     int navio_vertical_x = 1;
     int navio_vertical_y_start = 0;
@@ -19,7 +18,7 @@ int main() {
     printf("Coordenadas do navio vertical:\n");
     for (int i = 0; i < navio_vertical_tamanho; i++) {
         int y = navio_vertical_y_start + i;
-        tabuleiro[y][navio_vertical_x] = 1;
+        tabuleiro_novato[y][navio_vertical_x] = 1;
         printf("(%d, %d)\n", navio_vertical_x, y);
     }
 
@@ -27,11 +26,12 @@ int main() {
     printf("\nCoordenadas do navio horizontal:\n");
     for (int i = 0; i < navio_horizontal_tamanho; i++) {
         int x = navio_horizontal_x_start + i;
-        tabuleiro[navio_horizontal_y][x] = 1;
+        tabuleiro_novato[navio_horizontal_y][x] = 1;
         printf("(%d, %d)\n", x, navio_horizontal_y);
     }
 
-    // Etapa 3: Finalização
-    // O tabuleiro foi preenchido com os navios e as coordenadas foram exibidas
+    // Nível Aventureiro – Tabuleiro 10x10
+    int tabuleiro_aventureiro[10][10] = {0};
 
     return 0;
+}
